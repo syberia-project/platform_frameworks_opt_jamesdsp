bb062018b2bed01075ef7ad45873925c7d96e0f6
Christos Papageorgiou (root-expert) <root.expert.xda@gmail.com>
2018-11-25 21:05:17 +0100

    JamesDSP: Android.mk cleanup.
    * Remove LOCAL_PRELINK_MODULE := false
    * Remove commented out source files.
    * Add new lines to make the makefile nicer.

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,7 +1,9 @@
 LOCAL_PATH := $(call my-dir)
+
 include $(CLEAR_VARS)
+
 LOCAL_MODULE := libjamesdsp
-LOCAL_PRELINK_MODULE := false
+
 LOCAL_SRC_FILES := \
 	kissfft/kiss_fft.c \
 	kissfft/kiss_fftr.c \
@@ -17,10 +19,6 @@ LOCAL_SRC_FILES := \
 	vdc.c \
 	bs2b.c \
 	valve/12ax7amp/Tube.c \
-	valve/12ax7amp/wdfcircuits_triode.c \
-#	valve/wavechild670/amplifiers.c \
-#	valve/wavechild670/wdfcircuits.c \
-#	valve/wavechild670/wavechild670.c \
-# terminator
-#LOCAL_LDLIBS := -llog
+	valve/12ax7amp/wdfcircuits_triode.c
+
 include $(BUILD_SHARED_LIBRARY)
